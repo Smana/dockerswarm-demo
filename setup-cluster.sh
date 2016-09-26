5b6942c5c096dfdb215cbe2f653e23ac592e7738
Smana <smainklh@gmail.com>
2016-09-26 14:32:26 +0200

    add setup.sh script which brings up the cluster on GCE

---
+++ setup-cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+for m in {1..3}; do
+  docker-machine create -d google --google-project kargo-ci-1 manager${m}
+  declare MANAGER${m}_IP=$(docker-machine ssh manager${m} 'ifconfig eth0 | grep "inet addr:" | cut -d: -f2 | cut -d" " -f1')
+  export MANAGER${m}_IP
+done
+for i in {1..3}; do
+  docker-machine create -d google --google-project kargo-ci-1 worker${i}
+  declare WORKER${m}_IP=$(docker-machine ssh worker${m} 'ifconfig eth0 | grep "inet addr:" | cut -d: -f2 | cut -d" " -f1')
+  export WORKER${m}_IP
+done
+
+docker-machine ssh manager1 sudo docker swarm init --advertise-addr ${MANAGER1_IP}
+WORKER_TOKEN=$(docker-machine ssh manager1 sudo docker swarm join-token -q worker)
+MANAGER_TOKEN=$(docker-machine ssh manager1 sudo docker swarm join-token -q manager)
+
+for i in {2..3}; do
+  docker-machine ssh manager${i} sudo docker swarm join --token ${MANAGER_TOKEN} ${MANAGER1_IP}:2377
+done
+  
+
+for i in {1..3};do
+  docker-machine ssh worker${i} sudo docker swarm join --token ${WORKER_TOKEN} ${MANAGER1_IP}:2377
+done
+
+eval $(docker-machine env manager1)
